384547210394efd9bfa75cf17c3e8fbeb0cb23f0
Grigary1 <grigaryvarkey16@gmail.com>
2024-12-21 18:40:50 +0530

    initial commit

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Text } from 'react-native-web';
+import { Text, View } from 'react-native-web';
 
 function home() {
   return (
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,8 @@
+import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
+
   return (
     <Stack screenOptions={{
       headerShown:false
